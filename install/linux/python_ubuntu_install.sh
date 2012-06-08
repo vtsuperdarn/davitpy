b29e396ae2d4d7bb9c30d7e07d96a29f873d57df
Sebastien de Larquier <sebastien.delarquier@vt.edu>
2012-06-08 13:38:12 -0400

    created linux install script for python and required packages

--- install/linux/python_ubuntu_install.sh
+++ install/linux/python_ubuntu_install.sh
@@ -1,6 +1,12 @@
 # Python install script for Ubuntu
-# 	installs all pre-requisite software to run DaViT-py
-#	requires MacPort
+#	installs all pre-requisite software to run DaViT-py
+#	tested on Ubuntu 12.04
 
-ver=27
+ver=2.7
 
+apt-get install python$ver
+apt-get install ipython
+apt-get install python-numpy
+apt-get install python-scipy
+apt-get install python-matplotlib
+apt-get install python-h5py
