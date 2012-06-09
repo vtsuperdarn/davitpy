ec385b0c8562aeeb02f3c163203af32d2d6a6410
Sebastien de Larquier <sdelarquier@vt.edu>
2012-06-09 05:46:22 -0400

    comments

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,12 @@
 # Utils module __init__.py
+"""
+This subpackage contains various utilities for DaViT-py
+
+This includes the following functions:
+	parseDate
+	parseTime
+
+"""
 
 from parseDate import parseDate
 from parseTime import parseTime
