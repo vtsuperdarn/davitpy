c6d1609634d7df46c18d4b925707fbe0f9af1e2b
Sebastien de Larquier <sdelarquier@vt.edu>
2012-06-09 03:43:58 -0400

    configuration and read me for install script and launcher
    Still needs to be worked out

--- install/mac/set_paths_mac.sh
+++ install/mac/set_paths_mac.sh
@@ -6,9 +6,3 @@ if [ "" == "$DAVITPY" ]
 then
         echo "export DAVITPY="$tmpdavitpy >> ~/.bash_profile
 fi
-
-# Add ipython_dir to the environment variables
-if [ "" == "$IPYTHON_DIR" ]
-then
-        echo "export IPYTHON_DIR=~/.ipython" >> ~/.bash_profile
-fi
