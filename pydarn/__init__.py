bb642f90922277e712e7a38ce3adcd5dbc5ceab4
Sebastien de Larquier <sdelarquier@vt.edu>
2012-06-09 05:58:24 -0400

    reorganized into one main pydarn module

---
+++ pydarn/__init__.py
@@ -0,0 +1,15 @@
+# Main DaViT-py module __init__.py
+"""
+*******************************
+            UTILS
+*******************************
+This subpackage contains various utilities for DaViT-py
+
+This includes the following functions:
+	parseDate
+	parseTime
+
+*******************************
+"""
+
+from sdpy import utils
