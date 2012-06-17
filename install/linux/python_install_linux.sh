61182f1aa2c650bffccd6a737bc005af3fe21882
Sebastien de Larquier <sdelarquier@vt.edu>
2012-06-16 22:21:37 -0400

    added baseman to install scripts
    apparently baseman is not installed by default with matplotlib, so I
    added a line to each install scripts to install the required packages

--- install/linux/python_install_linux.sh
+++ install/linux/python_install_linux.sh
@@ -9,6 +9,7 @@ apt-get install ipython
 apt-get install python-numpy
 apt-get install python-scipy
 apt-get install python-matplotlib
+apt-get install python-mpltoolkits.basemap
 apt-get install python-h5py
 apt-get install python-tornado
 apt-get install python-zmq
--- install/mac/python_install_mac.sh
+++ install/mac/python_install_mac.sh
@@ -7,6 +7,7 @@ ver=27
 port install python${ver}
 port install py${ver}-numpy
 port install py${ver}-matplotlib
+port install py${ver}-matplotlib-basemap
 port install py${ver}-scipy
 port install py${ver}-h5py
 port install py${ver}-tornado
