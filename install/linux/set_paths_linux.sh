150911347873476d2636f5d95e5106585bb9861a
Sebastien de Larquier <sdelarquier@vt.edu>
2012-07-18 13:37:10 -0400

    install scripts minor bugs
    Corrected some minor bugs in the set_oaths scripts, as del as the
    python mac install.

--- install/linux/set_paths_linux.sh
+++ install/linux/set_paths_linux.sh
@@ -5,6 +5,7 @@ tmpdavitpy="`echo ${tmp%%davitpy/*}`"/davitpy
 if [ "" == "$DAVITPY" ]
 then
 	echo "# DaViTpy environment variables" >> ~/.bashrc
-        echo "export DAVITPY='"${tmpdavitpy}"'" >> ~/.bashrc
+    echo "export DAVITPY='"${tmpdavitpy}"'" >> ~/.bashrc
 	echo "export PATH="${DAVITPY}"/bin:\${PATH}" >> ~/.bashrc
+	echo "You can now check ~/.bashrc to make sure the path has been updated, then restart your terminal."
 fi
--- install/mac/set_paths_mac.sh
+++ install/mac/set_paths_mac.sh
@@ -4,7 +4,8 @@ tmp="`echo $(which davitpy)`"
 tmpdavitpy="`echo ${tmp%%davitpy/*}`"/davitpy
 if [ "" == "$DAVITPY" ]
 then
-        echo "# DaViTpy environment variables:" >> ~./bash_profile
+    echo "# DaViTpy environment variables:" >> ~/.bash_profile
 	echo "export DAVITPY='"$tmpdavitpy"'" >> ~/.bash_profile
-	echo "export PATH="${DAVITPY}"/bin:\${PATH}" >> ~/.bash_profile
+	echo "export PATH=\${DAVITPY}/bin:\${PATH}" >> ~/.bash_profile
+	echo "You can now check ~/.bash_profile to make sure the path has been updated, then restart your terminal."
 fi
