c035b218b02440d7091d58d74255eba179c3ed96
AJ Ribeiro <ajribeiro86@gmail.com>
2012-07-20 16:06:13 -0400

    can now read prm

--- pydarn/io/radDataRead.py
+++ pydarn/io/radDataRead.py
@@ -1,8 +1,9 @@
-import os,datetime,glob,math,shutil,string,time
+import os,datetime,glob,math,shutil,string,time,pydarn
 
 def radDataRead(dateStr,rad,times=[0,2400],fileType=0):
 	import math,glob,os,shutil,string,time
 	import pydarn.pydmap
+	import pydarn
 
 	#get the year of the file
 	yrStr = dateStr[0:4]
@@ -26,6 +27,7 @@ def radDataRead(dateStr,rad,times=[0,2400],fileType=0):
 	hr2 = times[1]/100.
 	hr1 = int(math.floor(hr1/2.)*2)
 	hr2 = int(math.floor(hr2/2.)*2)
+	
 	
 	#a temporary directory to store a temporary file
 	tmpDir = '/tmp/fit/'
@@ -77,8 +79,33 @@ def radDataRead(dateStr,rad,times=[0,2400],fileType=0):
 	for filename in filelist:
 		os.system('rm '+filename)
 		
-	return dfile
+	
+	myRadData = pydarn.io.radData()
+	
+	ftimes = dfile.times
+	
+	stime = datetime.datetime(int(dateStr[0:4]),int(dateStr[4:6]),int(dateStr[6:8]),int(math.floor(times[0]/100.)),int((times[0]/100.-math.floor(times[0]/100.))*100))
+	etime = datetime.datetime(int(dateStr[0:4]),int(dateStr[4:6]),int(dateStr[6:8]),int(math.floor(times[1]/100.)),int((times[1]/100.-math.floor(times[1]/100.))*100))
+
+	for t in ftimes:
+		if(t >= stime and t <= etime):
+			print stime,t,etime
+			myBeam = pydarn.io.beam()
 		
+			myPrmData = parsePrm(dfile[t])
+			myBeam['prm'] = myPrmData
+			myRadData[t] = myBeam
 		
-	#delete the temporary file
-	#os.system('rm'+tmpName)
+	return myRadData
+
+	
+def parsePrm(rec):
+	
+	myPrmData = pydarn.io.prmData()
+	
+	for k in myPrmData.iterkeys():
+		if(rec.has_key(k)):
+			myPrmData[k] = rec[k]
+	
+	return myPrmData
+	
