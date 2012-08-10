000d0dbaad6ca5b799e410a402898448e4c12313
Nathaniel Frissell <nafrissell@vt.edu>
2012-08-10 15:38:19 -0400

    Testing...

--- mstid/test.py
+++ mstid/test.py
@@ -3,7 +3,8 @@ import numpy
 from matplotlib import pyplot
 import spacepy
 import spacepy.time as spt
-
+import datetime
+import dateutil.parser
 #import matplotlib as mplot
 
 print "Hello World."
@@ -14,17 +15,28 @@ psd_node = f['doyPsdVec']
 
 jul = jul_node.value
 psd = psd_node.value
+t = spt.Ticktock(jul,'JD')
+#ep = t.
 
+now = datetime.datetime.now()
+a = range(len(psd))
+b = [now + datetime.timedelta(x) for x in a]
 #NaN = numpy.NAN
 
+
+
 pyplot.interactive(True)
-pyplot.figure(1)
-pyplot.plot(psd)
+pyplot.close('all')
+fig = pyplot.figure(1)
+#pyplot.title('MSDTID Occurence')
+#ax1 = fig.add_subplot(241)
+pyplot.plot(b,psd)
+pyplot.xlabel('Day of Year')
+pyplot.ylabel('Integrated PSD')
+pyplot.title('GBR')
 #pyplot.show()
-#pyplot.close('all')
-pyplot.savefig('foo.eps')
-pyplot.savefig('foo.pdf')
-pyplot.savefig('foo.png')
+#pyplot.savefig('foo.eps')
+#pyplot.savefig('foo.pdf')
+#pyplot.savefig('foo.png')
+#
 
-
-t = spt.Ticktock(jul,'JD')
