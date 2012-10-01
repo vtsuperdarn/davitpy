3087b86d2a7ec5643a149671710e0a3cb419f3f6
Nathaniel A. Frissell <nafrissell@vt.edu>
2012-10-01 10:10:49 -0400

    Created proc.signal module.

---
+++ pydarn/proc/signal/io.py
@@ -0,0 +1,39 @@
+# WAVER
+
+def readsav(radar,date,time,param,bandLim,procType,dataDir):
+	"""
+	*******************************
+	
+        dataObj = readsav(radar,date,time,param,bandlim)
+	
+	takes a python datetime object and returns a string in yyyymmdd format
+
+	INPUTS:
+		myDate : a python date object
+	OUTPUTS:
+		dateStr : a string in yyyymmdd format
+
+	Written by Nathaniel 14AUG2012
+	*******************************
+	"""
+
+        from scipy.io.idl import readsav
+        import numpy as np
+        import os.path
+         
+        dateSt = str(date[0])
+        timeSt = '.'.join(["%s" % el for el in time])
+        bandLim = np.array(bandLim) * 1.e6
+        bandSt = '-'.join(["%i" % el for el in bandLim])
+
+        a = [dateSt,radar,param,bandSt,procType,'sav']
+        fileName = '.'.join(a)
+
+        path = '/'.join([dataDir,fileName])
+
+        if os.path.exists(path):
+          dataObj = readsav(path)
+          return dataObj
+        else:
+          print ' '.join([fileName,'does not exist.'])
+          sys.exit()
