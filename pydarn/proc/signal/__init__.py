1f8d29a35f0181ef739175ba59c1d7dcca7949c6
Nathaniel A. Frissell <nafrissell@vt.edu>
2012-10-03 15:20:34 -0400

    Added detrending

--- pydarn/proc/signal/__init__.py
+++ pydarn/proc/signal/__init__.py
@@ -12,7 +12,7 @@ DEV: functions/modules/classes with a * have not been developed yet
 """
 import sigio
 from signal import *
-from filter import *
+from sigproc import *
 
 
 # *************************************************************
