1bba3bd8d09dfa73318d84670efaf4db42ff50cb
AJ Ribeiro <ajribeiro86@gmail.com>
2013-01-22 22:43:46 -0500

    started omni read code

---
+++ gmi/omni/__init__.py
@@ -0,0 +1,20 @@
+# Overall module for gmi (Geo Magnetic Indices)
+"""
+*******************************
+            OMNI
+*******************************
+Module with libraries for dealing with omni
+
+This includes the following submodules:
+	omni
+		omni index tools
+
+
+*******************************
+"""
+
+try: import omni
+except Exception,e: print e
+
+try: from omni import *
+except Exception,e: print e
