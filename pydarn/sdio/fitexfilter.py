fixed azimuth calculation in pygridlib

pydarn/sdio/fitexfilter.py
@@ -1,5 +1,7 @@
 import pydarn
 import numpy as np
+import datetime as dt
+import utils
 
 def combBeams(scan):
   """This function combines all repeated beams within a scan into an averaged beam
@@ -43,7 +45,8 @@ def combBeams(scan):
 
       #initialize a new beam object
       myBeam.copyData(beams[0])
-      for f in myBeam.fit: f = []
+      for key,val in myBeam.fit.__dict__.iteritems(): 
+        setattr(myBeam.fit,key,[])
       myBeam.prm.nrang = nrang
 
       for j in range(nrang):
@@ -65,9 +68,56 @@ def combBeams(scan):
       outScan.append(myBeam)
 
   sorted(outScan, key=lambda beam: beam.bmnum)
+
   return outScan
-  
-def fitFilter(scans,thresh=.4):
+
+class Gate(object):
+  def __init__(self,fit,i):
+    self.v = fit.v[i]
+    self.w_l = fit.w_l[i]
+    self.p_l = fit.p_l[i]
+    self.pwr0 = fit.pwr0[i]
+    if fit.elv != None: self.elv = fit.elv[i]
+    else: self.elv = None
+    if fit.phi0 != None: self.phi0 = fit.phi0[i]
+    else: self.phi0 = None
+
+def fitFilter(inFile,outFile,thresh=0.4):
+  inp = pydarn.sdio.radDataOpen(dt.datetime(2010,5,1),'bks',fileName=inFile)
+
+  outp = open(outFile,'w')
+
+  scans = [None, None, None]
+
+  sc = pydarn.sdio.radDataReadScan(inp)
+  scans[1] = sc
+
+  sc = pydarn.sdio.radDataReadScan(inp)
+  scans[2] = sc
+
+  while sc != None:
+
+    tsc = doFilter(scans,thresh=thresh)
+
+    # for b in tsc:
+    #   print b
+    #   pydarn.dmapio.writeFitRec(b,utils.datetimeToEpoch(b.time),outp)
+
+    sc = pydarn.sdio.radDataReadScan(inp)
+    
+    scans[0] = scans[1]
+    scans[1] = scans[2]
+    scans[2] = sc
+
+  tsc = doFilter(scans,thresh=thresh)
+  print tsc.time
+  # for b in tsc:
+  #   pydarn.dmapio.writeFitRec(b,utils.datetimeToEpoch(b.time),outp)
+
+  outp.close()
+
+
+def doFilter(scans,thresh=.4):
 
   myScans = []
   for s in scans:
@@ -76,9 +126,114 @@ def fitFilter(scans,thresh=.4):
     else:
       myScans.append(combBeams(s))
 
+  outScan = pydarn.sdio.scanData()
+
+  #define the weigths array
+  w = [[[0. for i in range(3)] for j in range(3)] for k in range(3)]
+  for i in range(0,3):
+    for j in range(0,3):
+      for k in range(0,3):
+        if k == 0: tplus = 1
+        else: tplus = 0
+        if i == 0: rplus = 1
+        else: rplus = 0
+        if j == 0: bmplus = 1
+        else: bmplus = 0
+        if i == 0 and j == 0 and k == 0: centplus = 1
+        else: centplus = 0
+
+        w[(i+1)%3][(j+1)%3][(k+1)%3] = 1+tplus+rplus+bmplus+centplus
+
+
+
   for b in myScans[1]:
-    for i in range(b.fit.slist)
-      box = [[[None for i in range(3)] for j in range(3)] for k in range(3)]
+    bmnum = b.bmnum
+    #make a new beam
+    myBeam = pydarn.sdio.beamData()
+    myBeam.copyData(b)
+    for key,val in myBeam.fit.__dict__.iteritems(): 
+      setattr(myBeam.fit,key,[])
+
+    for r in range(0,b.prm.nrang):
+
+      #boxcar to hold the gates
+      box = [[[None for j in range(3)] for k in range(3)] for n in range(3)]
+
+      #iterate through time
+      for j in range(0,3):
+        #iterate through beam
+        for k in range(-1,2):
+          #iterate through gate
+          for n in range(-1,2):
+
+            #get the scan we are working on
+            s = myScans[j]
+            if s == None: continue
+
+            #get the beam we are working on
+            tbm = None
+            for bm in s:
+              if bm.bmnum == bmnum + k: tbm = bm
+            if tbm == None: continue
+
+            #check if target gate number is in the beam
+            if r+n in tbm.fit.slist:
+              ind = tbm.fit.slist.index(r+n)
+              box[j][k+1][n+1] = Gate(tbm.fit,ind)
+            else: box[j][k+1][n+1] = 0
+
+      pts,tot=0.,0.
+      v,w_l,p_l,elv,phi0,pwr0 = [],[],[],[],[],[]
+      #iterate through time
+      for j in range(0,3):
+        #iterate through beam
+        for k in range(0,3):
+          #iterate through gate
+          for n in range(0,3):
+            bx = box[j][k][n]
+            if bx == None: continue
+            wt = w[j][k][n]
+            tot += wt
+            if bx != 0:
+              pts += wt
+              for m in range(0,wt):
+                v.append(bx.v)
+                pwr0.append(bx.pwr0)
+                w_l.append(bx.w_l)
+                p_l.append(bx.p_l)
+                if bx.elv: elv.append(bx.elv)
+                if bx.phi0: phi0.append(bx.phi0)
+
+      #check if we meet the threshold
+      if pts/tot >= thresh:
+        myBeam.fit.slist.append(r)
+        myBeam.fit.qflg.append(1)
+        myBeam.fit.v.append(np.median(v))
+        myBeam.fit.w_l.append(np.median(w_l))
+        myBeam.fit.p_l.append(np.median(p_l))
+        myBeam.fit.pwr0.append(np.median(pwr0))
+        if elv != []: myBeam.fit.elv.append(np.median(elv))
+        if phi0 != []: myBeam.fit.phi0.append(np.median(phi0))
+        if np.median(w_l) > -3.*np.median(v)+90.:
+          myBeam.fit.gflg.append(0)
+        else: myBeam.fit.gflg.append(1)
+
+    outScan.append(myBeam)
+
+  return outScan
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 