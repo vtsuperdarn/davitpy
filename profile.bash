e8b33f3c42f53115d388be842123a815df7ce01f
Sebastien de Larquier <sebastien.delarquier@gmail.com>
2013-06-10 12:58:37 -0400

    removed writeuser from profile.bash

--- profile.bash
+++ profile.bash
@@ -13,18 +13,22 @@ export PATH=${DAVITPY}/bin:${PATH}
 # Set path to DAVITPY
 export PYTHONPATH=$PYTHONPATH:$DAVITPY
 
-
 # Set Database users
 export DBREADUSER='sd_dbread'
 export DBREADPASS='5d'
-
-# Set Database users
-export DBWRITEUSER=''
-export DBWRITEPASS=''
-
 
 # Set Database users
 export SDDB='sd-work9.ece.vt.edu:27017'
 
 # Set SFTP DATABASE
 export VTDB='sd-data.ece.vt.edu'
+
+# Set data path and format
+# UAF
+#export DAVIT_DIRFORMAT='%(dirtree)s/%(ftype)s/%(year)s/%(month)s.%(day)s/'
+#export DAVIT_LOCALDIR=/raid/SuperDARN/data/
+# VT
+export DAVIT_DIRFORMAT='%(dirtree)s/%(year)s/%(ftype)s/%(radar)s/'
+export DAVIT_LOCALDIR=/sd-data/
+
+
