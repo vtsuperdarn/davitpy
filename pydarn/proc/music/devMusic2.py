5b4404d2be2f1d7805a9e15894ddf2babff8f4af
Nathaniel A. Frissell <nafrissell@vt.edu>
2013-07-31 15:57:20 -0400

    created a second part to the devMusic script

---
+++ pydarn/proc/music/devMusic2.py
@@ -0,0 +1,33 @@
+############################################
+# This code adds davitpy to your python path
+# Eventually, this won't be necessary
+import sys
+sys.path.append('/davitpy')
+############################################
+
+# <codecell>
+
+import matplotlib
+matplotlib.use('Agg')
+
+import pydarn.sdio
+import datetime
+import numpy as np
+
+# <codecell>
+
+import pydarn.proc.music as music
+
+from matplotlib import pyplot as plt
+
+import pickle
+
+outdir = '/data/pymusic/'
+figsize = (20,10)
+
+
+dataObj = pickle.load(open('dataObj.p','rb'))
+
+fig = plt.figure(figsize=figsize)
+pydarn.plotting.musicPlot.plotKarr(dataObj,fig=fig)
+fig.savefig(outdir+'/karr.png')
