99c32702254e7586e8bac2fdf0bbcc0d294bc257
sdelarquier <sebastien.delarquier@gmail.com>
2013-09-29 12:35:32 -0700

    added brew install script for mac

---
+++ install/mac/python_install_mac_brew.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Python install script for mac
+#   installs all pre-requisite software to run DaViT-py
+#   requires HomeBrew
+
+# Non-python dependencies
+brew install gfortran
+brew install mpich2
+brew install coreutils
+brew install pkg-config
+brew install freetype
+brew install libpng
+brew install python
+# Python dependencies
+easy_install pip
+brew install readline
+brew install zeromq
+pip install --upgrade pyzmq tornado pygments
+pip install --upgrade ipython
+python -c 'from IPython.external import mathjax; mathjax.install_mathjax()'
+pip install --upgrade numpy
+pip install --upgrade matplotlib
+pip install --upgrade scipy
+pip install --upgrade h5py
+pip install --upgrade PIL
+pip install --upgrade pymongo
+pip install --upgrade paramiko
+
+dir=$(pwd)
+cd /tmp
+git clone https://github.com/matplotlib/basemap.git
+python2.7 setup.py install
+
+
+cd $dir
+install_dir=$(greadlink -f ../..)
+echo "source $install_dir/profile.mac" >> ~/.bash_profile
+
+
+cd ../..
+./mastermake
