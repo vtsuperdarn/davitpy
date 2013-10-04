7d882c0914a956d40e3f25150fb6696438ebaec7
AJ Ribeiro <ajribeiro86@gmail.com>
2013-10-04 14:17:09 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+
+sources = []
+
+################################################################################
+# get a list of all source files
+def get_files(dir):
+    for f in os.listdir(dir):
+        if os.path.isdir(dir+'/'+f) and '__init__.py' in os.listdir(dir+'/'+f):
+            sources.append(dir+'/'+f)
+            get_files(dir+'/'+f)
+get_files(os.getcwd())
+################################################################################
+
+
+
+
+setup(name='davitpy',
+       version = "0.1",
+       description = "data visualization toolkit-python",
+       author = "VT SuperDARN Lab and friends",
+       author_email = "ajribeiro86@gmail.com",
+       url = "https://github.com/vtsuperdarn/davitpy",
+       packages=[x.replace(os.getcwd()+'/','') for x in sources],
+       install_requires=['ipython','numpy','scipy','matplotlib','tornado','zmq', \
+                        'paramiko','pymongo','h5py','mechanize','basemap']
+      )
+
+
+
+# sudo apt-get install libfreetype6-dev
+# sudo apt-get install libpng-dev
+# apt-get install -y mpich2
+# apt-get install -y gfortran
+
+
