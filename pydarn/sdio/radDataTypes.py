94531ef58326f0cff57332f515ecad6482216809
asreimer <ashtonsethreimer@gmail.com>
2014-07-23 12:07:23 -0600

    Update radDataTypes.py
    Additional imports missing. Without these, we cannot fetch files using sftp.

--- pydarn/sdio/radDataTypes.py
+++ pydarn/sdio/radDataTypes.py
@@ -75,7 +75,9 @@ class radDataPtr():
     import os,glob,string
     from pydarn.radar import network
     import utils
-
+    import paramiko as p
+    import re
+    
     self.sTime = sTime
     self.eTime = eTime
     self.stid = stid
