66c6340c34644dfbfd08f4c309102b526fb73fd8
Matthew Wessel <matt.wessel@usask.ca>
2014-08-09 15:15:26 -0600

    Making coordUtils comply with PEP 8 (without breaking compat).

--- utils/coordUtils.py
+++ utils/coordUtils.py
@@ -24,160 +24,215 @@
 **Module**: utils.coordUtils
 ****************************
 **Functions**:
-  * :func:`utils.coordUtils.coordConv`: Convert between geographic, AACGM, and MLT coordinates
+  * :func:`utils.coordUtils.coordConv`: Convert between geographic, 
+        AACGM, and MLT coordinates
   * :func:`utils.coordUtils.planeRot`: Rotate coordinates in the plane
 
 Written by Matt W. based on code by...Sebastien?
 """
 
-from models import aacgm
-import numpy as np
 
 def coordConv(lon, lat, start, end, dateTime=None):
-  """Convert between geographical, AACGM, and MLT coordinates.  dateTime must be set to use MLT.
+    """Convert between geographical, AACGM, and MLT coordinates.  
+        dateTime must be set to use any AACGM systems.
   
-  **Args**: 
-      * **lon**: longitude (MLT must be in degrees, not hours)
-      * **lat**: latitude
-      * **start**: coordinate system of input. Options: 'geo', 'mag', 'mlt'
-      * **end**: desired output coordinate system. Options: 'geo', 'mag', 'mlt'
-      * **[dateTime]**: python datetime object. Default: None
-  **Returns**:
-    * **lon,lat**: lists
-  **Example**:
-    ::
+    **Args**: 
+        * **lon**: longitude (MLT must be in degrees, not hours)
+        * **lat**: latitude
+        * **start**: coordinate system of input. Options: 'geo', 'mag',
+            'mlt'
+        * **end**: desired output coordinate system. Options: 'geo', 
+            'mag', 'mlt'
+        * **[dateTime]**: python datetime object. Default: None
+    **Returns**:
+        * **lon,lat**: lists
+    **Example**:
+        ::
+    
+        lon, lat = coordConv(lon, lat, 'geo', 'mlt', 
+                             dateTime=datetime(2012,3,12,0,56))
+        
+    written by Matt W., 2013-09 based on code by...Sebastien?
+    """
+    import numpy as np
+    
+    from models import aacgm
 
-      lon, lat = coordConv(lon, lat, 'geo', 'mlt', dateTime=datetime(2012,3,12,0,56))
-      
-  written by Matt W., 2013-09 based on code by...Sebastien?
-  """
-  # Define acceptable coordinate systems
-  coordsDict = {'mag': 'AACGM',
-            'geo': 'Geographic',
-            'mlt': 'MLT'}
+    date_time = dateTime
 
-  # Check that the coordinates are possible
-  if end and end not in coordsDict:
-    print 'Invalid end coordinate system given ({}): setting "mag"'.format(end)
-    end = 'mag' # Default value
-  if start and start not in coordsDict:
-    print 'Invalid start coordinate system given ({}): setting "{}"'.format(start,end)
-    start = end 
+    # Define acceptable coordinate systems.
+    coords_dict = {"mag": "AACGM",
+                   "geo": "Geographic",
+                   "mlt": "MLT"}
+    
+    # Check that the coordinates are possible.
+    if end and end not in coords_dict:
+        print "Invalid end coordinate system given ({}):"
+        print "setting 'mag'".format(end)
+        end = "mag"
+    if start and start not in coords_dict:
+        print "Invalid start coordinate system given ({}):"
+        print "setting '{}'".format(start,end)
+        start = end 
+    
+    # MLT requires a datetime.
+    if start == "mlt" or end == "mlt": 
+        assert(date_time is not None),\
+                "date_time must be provided for MLT coordinates to work."
+    # Mag requires a datetime.
+    if start == 'mag' or end == 'mag': 
+        assert(date_time is not None),\
+                "date_time must be provided for MAG coordinates to work."
 
-  # MLT requires a datetime
-  if start == 'mlt' or end == 'mlt': 
-    assert(dateTime is not None),"dateTime must be provided for MLT coordinates to work."
-  # mag requires a datetime
-  if start == 'mag' or end == 'mag': 
-    assert(dateTime is not None),"dateTime must be provided for MAG coordinates to work."
+    # Make the inputs into numpy arrays because single element lists 
+    # have no len.
+    lon = np.array(lon)
+    lat = np.array(lat)
 
-  # Make the inputs into numpy arrays because single element lists have no 'len'
-  lon = np.array(lon)
-  lat = np.array(lat)
+    # If there is an actual conversion to do:
+    if start and end and start != end:
+        # Set the conversion specifier.
+        trans = start+'-'+end
 
-  # If there is an actual conversion to do...
-  if start and end and start != end:
-    trans = start+'-'+end                     # Set the conversion specifier
-    # Geographical and AACGM conversions
-    if trans in ['geo-mag','mag-geo']:
-      flag = 0 if trans == 'geo-mag' else 1
-      lont = lon
-      latt = lat
-      nlon, nlat = np.size(lont), np.size(latt)   # Sizes
-      shape = lont.shape                          # Shape of array
-      lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), list(lont.flatten()), [0.]*nlon, dateTime.year,flag) # Convert either way
-      lon = np.array(lon).reshape(shape)        # Put results into numpy array and reshape
-      lat = np.array(lat).reshape(shape)
-    # Geographical and MLT conversions
-    elif trans in ['geo-mlt','mlt-geo']:
-      flag = 0 if trans == 'geo-mlt' else 1
-      if flag:
-        latt = lat
-        lon_mlt = lon
-        shape = lon_mlt.shape    
-        nlon, nlat = np.size(lon_mlt), np.size(latt)
-        lon_mag = []                                              # Mag lon list
-        mlt_0 = aacgm.mltFromYmdhms(dateTime.year,dateTime.month,dateTime.day,
-            dateTime.hour,dateTime.minute,dateTime.second,0.)     # Find MLT at zero mag lon
-        for el in range(nlon):
-          mlt = lon_mlt.flatten()[el]*24./360.%24.                # Convert input mlt from degrees to hours
-          mag = (mlt - mlt_0)*15.%360.                            # Calculate mag lon
-          if mag > 180.: mag -= 360.                              # Put in -180 to 180 range
-          lon_mag.append(mag)                                     # Stick on end of the list
-        lont = np.array(lon_mag).reshape(shape)                   # Make into a numpy array
-        lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), list(lont.flatten()), [0.]*nlon, dateTime.year, flag) # Convert mag to geo
-        lon = np.array(lon).reshape(shape)                        # Make into numpy arrays
-        lat = np.array(lat).reshape(shape)
-      else:
-        lon_mlt = []                                              # MLT list
-        lont = lon
-        latt = lat
-        nlon, nlat = np.size(lont), np.size(latt)
-        shape = lont.shape    
-        lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), list(lont.flatten()), [0.]*nlon, dateTime.year, flag) # Convert geo to mag
-        for lonel in range(nlon):
-          mlt = aacgm.mltFromYmdhms(dateTime.year,dateTime.month,dateTime.day,
-              dateTime.hour,dateTime.minute,dateTime.second,lon[lonel]) # Get MLT from mag lon
-          lon_mlt.append(mlt/24.*360.)                            # Convert hours to degrees
-        lon = np.array(lon_mlt).reshape(shape)                    # Make into numpy arrays
-        lat = np.array(lat).reshape(shape)
-    elif trans in ['mag-mlt','mlt-mag']:
-      flag = 0 if trans == 'mag-mlt' else 1
-      if flag:
-        lon_mag=[]
-        lon_mlt = lon
-        nlon = np.size(lon_mlt)
-        shape = lon_mlt.shape
-        mlt_0 = aacgm.mltFromYmdhms(dateTime.year,dateTime.month,dateTime.day,
-            dateTime.hour,dateTime.minute,dateTime.second,0.)     # Find MLT of zero mag lon
-        for el in range(nlon):
-          mlt = lon_mlt.flatten()[el]*24./360.%24.                # Convert MLT from degrees to hours
-          mag = (mlt - mlt_0)*15.%360.                            # Calculate mag lon from MLT
-          if mag > 180.: mag -= 360.                              # Put in -180 to 180 range
-          lon_mag.append(mag)
-        lon = np.array(lon_mag).reshape(shape)
-        lat = lat.reshape(shape)
-      else:
-        lon_mlt = []
-        lont = lon
-        nlon = np.size(lont)
-        shape=lont.shape
-        for lonel in range(nlon):
-          mlt = aacgm.mltFromYmdhms(dateTime.year,dateTime.month,dateTime.day,
-              dateTime.hour,dateTime.minute,dateTime.second,lont.flatten()[lonel])  # Find MLT from mag lon and datetime
-          lon_mlt.append(mlt/24.*360.)                            # Convert hours to degrees
-        lon = np.array(lon_mlt).reshape(shape)
-        lat = lat.reshape(shape)
+        # Geographical and AACGM conversions:
+        if trans in ['geo-mag','mag-geo']:
+            flag = 0 if trans == 'geo-mag' else 1
+            lont = lon
+            latt = lat
+            nlon, nlat = np.size(lont), np.size(latt)
+            shape = lont.shape
+            lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), 
+                                             list(lont.flatten()), [0.]*nlon, 
+                                             date_time.year,flag)
+            lon = np.array(lon).reshape(shape)
+            lat = np.array(lat).reshape(shape)
 
-  return list(lon.flatten()),list(lat.flatten())
+        # Geographical and MLT conversions:
+        elif trans in ['geo-mlt','mlt-geo']:
+            flag = 0 if trans == 'geo-mlt' else 1
+            if flag:
+                latt = lat
+                lon_mlt = lon
+                shape = lon_mlt.shape
+                nlon, nlat = np.size(lon_mlt), np.size(latt)
+                lon_mag = []
+                # Find MLT at zero mag lon.
+                mlt_0 = aacgm.mltFromYmdhms(date_time.year,date_time.month,
+                                            date_time.day, date_time.hour,
+                                            date_time.minute, date_time.second,
+                                            0.)
+                for el in range(nlon):
+                    # Convert input mlt from degrees to hours.
+                    mlt = lon_mlt.flatten()[el]*24./360.%24.
+                    # Calculate mag lon.
+                    mag = (mlt - mlt_0)*15.%360.
+                    # Put in -180 to 180 range.
+                    if mag > 180.: mag -= 360.
+                    # Stick on end of the list.
+                    lon_mag.append(mag)
+                # Make into a numpy array.
+                lont = np.array(lon_mag).reshape(shape)
+                # Convert mag to geo.
+                lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), 
+                                                 list(lont.flatten()), 
+                                                 [0.]*nlon, date_time.year, 
+                                                 flag)
+                # Make into numpy arrays.
+                lon = np.array(lon).reshape(shape)
+                lat = np.array(lat).reshape(shape)
+            else:
+                # MLT list.
+                lon_mlt = []
+                lont = lon
+                latt = lat
+                nlon, nlat = np.size(lont), np.size(latt)
+                shape = lont.shape
+                # Convert geo to mag.
+                lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), 
+                                                 list(lont.flatten()), 
+                                                 [0.]*nlon, date_time.year, 
+                                                 flag)
+                for lonel in range(nlon):
+                    # Get MLT from mag lon.
+                    mlt = aacgm.mltFromYmdhms(date_time.year,date_time.month,
+                                              date_time.day, date_time.hour,
+                                              date_time.minute,
+                                              date_time.second, lon[lonel]) 
+                    # Convert hours to degrees.
+                    lon_mlt.append(mlt/24.*360.)
+                # Make into numpy arrays.
+                lon = np.array(lon_mlt).reshape(shape)
+                lat = np.array(lat).reshape(shape)
+
+        # Magnetic and MLT conversions:
+        elif trans in ['mag-mlt','mlt-mag']:
+            flag = 0 if trans == 'mag-mlt' else 1
+            if flag:
+                lon_mag=[]
+                lon_mlt = lon
+                nlon = np.size(lon_mlt)
+                shape = lon_mlt.shape
+                # Find MLT of zero mag lon.
+                mlt_0 = aacgm.mltFromYmdhms(date_time.year, date_time.month,
+                                            date_time.day, date_time.hour,
+                                            date_time.minute, date_time.second,
+                                            0.)     
+                for el in range(nlon):
+                    # Convert MLT from degrees to hours.
+                    mlt = lon_mlt.flatten()[el]*24./360.%24.
+                    # Calculate mag lon from MLT.
+                    mag = (mlt - mlt_0)*15.%360.
+                    # Put in -180 to 180 range.
+                    if mag > 180.: mag -= 360.
+                    lon_mag.append(mag)
+                lon = np.array(lon_mag).reshape(shape)
+                lat = lat.reshape(shape)
+            else:
+                lon_mlt = []
+                lont = lon
+                nlon = np.size(lont)
+                shape=lont.shape
+                for lonel in range(nlon):
+                    # Find MLT from mag lon and datetime.
+                    mlt = aacgm.mltFromYmdhms(date_time.year, date_time.month,
+                            date_time.day, date_time.hour, date_time.minute,
+                            date_time.second, lont.flatten()[lonel])
+                    # Convert hours to degrees.
+                    lon_mlt.append(mlt/24.*360.)
+                lon = np.array(lon_mlt).reshape(shape)
+                lat = lat.reshape(shape)
+        
+    return list(lon.flatten()), list(lat.flatten())
 
 ########################################################################
 ########################################################################
 
 def planeRot(x, y, theta):
-  """Rotate coordinates in the plane.
+    """Rotate coordinates in the plane.
   
-  **Args**: 
-      * **x**: x coordinate
-      * **y**: y coordinate
-      * **theta**: angle of rotation of new coordinate frame
-  **Returns**:
-    * **x_prime,y_prime**: x, y coordinates in rotated frame
-  **Example**:
-    ::
+    **Args**: 
+        * **x**: x coordinate
+        * **y**: y coordinate
+        * **theta**: angle of rotation of new coordinate frame
+    **Returns**:
+        * **x_prime,y_prime**: x, y coordinates in rotated frame
+    **Example**:
+        ::
+    
+        import numpy as np
+        x, y = planeRot(x, y, 30.*np.pi/180.)
+        
+    written by Matt W., 2013-09
+    """
+    import numpy as np
 
-      import numpy as np
-      x, y = planeRot(x, y, 30.*np.pi/180.)
-      
-  written by Matt W., 2013-09
-  """
-  oldx, oldy = np.array(x), np.array(y)
-  x = oldx*np.cos(theta) + oldy*np.sin(theta)
-  y = -oldx*np.sin(theta) + oldy*np.cos(theta)
-  return x, y
+    oldx, oldy = np.array(x), np.array(y)
+    x = oldx*np.cos(theta) + oldy*np.sin(theta)
+    y = -oldx*np.sin(theta) + oldy*np.cos(theta)
+    return x, y
 
 
-# Some testing stuff
+# Some testing stuff.
 if __name__ == "__main__":
     from datetime import datetime
 
