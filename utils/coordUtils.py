8dac12283db367e141fa59d54fb778d9b66e5a69
Matthew Wessel <matt.wessel@usask.ca>
2014-08-11 11:42:22 -0600

    Fixed bugs and switched to (-180,180) system for longitude.

--- utils/coordUtils.py
+++ utils/coordUtils.py
@@ -16,7 +16,7 @@
 
 """
 .. module:: coordUtils
-   :synopsis: A module for manipulating coordinates
+    :synopsis: A module for manipulating coordinates
 
 .. moduleauthor:: mrwessel
 
@@ -24,14 +24,27 @@
 **Module**: utils.coordUtils
 ****************************
 **Functions**:
-  * :func:`utils.coordUtils.coordConv`: Convert between geographic, 
+    
+    * :func:`utils.coordUtils.coordConv`: Kept for back-compat, calls
+        coord_conv
+    * :func:`utils.coordUtils.coord_conv`: Convert between geographic, 
         AACGM, and MLT coordinates
-  * :func:`utils.coordUtils.planeRot`: Rotate coordinates in the plane
+    * :func:`utils.coordUtils.planeRot`: Rotate coordinates in the plane
 
-Written by Matt W. based on code by...Sebastien?
+Written by Matt W.
 """
-
 def coordConv(lon, lat, altitude, start, end, dateTime=None):
+    """coordConv has been renamed coord_conv, and dateTime has been 
+        renamed date_time for PEP 8 compliance.  Please use those 
+        from now on.
+    """
+    from utils.coordUtils import coord_conv
+    print "coordConv has been renamed coord_conv, and dateTime has"
+    print "been renamed date_time for PEP 8 compliance.  Please use"
+    print "those from now on."
+    return coord_conv(lon, lat, altitude, start, end, date_time=dateTime)
+
+def coord_conv(lon, lat, altitude, start, end, date_time=None):
     """Convert between geographical, AACGM, and MLT coordinates.  
         dateTime must be set to use any AACGM systems.
   
@@ -43,22 +56,23 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
             'mlt'
         * **end**: desired output coordinate system. Options: 'geo', 
             'mag', 'mlt'
-        * **[dateTime]**: python datetime object. Default: None
+        * **[date_time]**: python datetime object. Default: None
     **Returns**:
-        * **lon,lat**: lists
+        * **lon, lat**: (float, list, or numpy array) MLT is in degrees, not
+            hours.  Output type is the same as input type (except int becomes
+            float)
     **Example**:
         ::
-    
-        lon, lat = coordConv(lon, lat, 'geo', 'mlt', 
-                             dateTime=datetime(2012,3,12,0,56))
+        import utils
+        lon, lat = utils.coord_conv(lon, lat, 'geo', 'mlt', 
+                             date_time=datetime(2012,3,12,0,56))
         
-    written by Matt W., 2013-09 based on code by...Sebastien?
+    original version written by Matt W., 2013-09 based on code by...Sebastien?
+    brand new version by Matt W., 2014-08
     """
     import numpy as np
     
     from models import aacgm
-
-    date_time = dateTime
 
     # Define acceptable coordinate systems.
     coords_dict = {"mag": "AACGM",
@@ -101,8 +115,11 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
     if isinstance(lon, int):
         lon = float(lon)
         lat = float(lat)
-    is_float = isinstance(lon, float)
     is_list = isinstance(lon, (list, tuple))
+    is_float = isinstance(lon, float)
+    if is_float:
+        lon = [lon]
+        lat = [lat]
     if not (is_float or is_list):
         assert(isinstance(lon, np.ndarray)),\
                 "Must input int, float, list, or numpy array."
@@ -110,10 +127,10 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
     # Make the inputs into numpy arrays because single element lists 
     # have no len.
     lon, lat = np.array(lon), np.array(lat)
-    nlon, nlat = np.size(lon), np.size(lat)
-    shape = lon.shape()
+    shape = np.shape(lon)
 
     # Test whether we are using the same altitude for everything.
+    alt = np.array(altitude)
     if np.size(alt) == 1:
         altitude = [altitude]*np.size(lon)
 
@@ -141,7 +158,8 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
                 # Sanitise and convert to degrees.
                 lon %= 24.
                 lon *= 360./24.
-                # Is there a need to convert to -180 180 from 0 360?
+                # Covert from (0,360) to (-180,180).
+                lon[np.where(lon > 180.)] -= 360.
                 start = "mag"
         
             # Now it is in AACGM.  
@@ -149,8 +167,8 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
 
             # If the end result is not an AACGM system, convert to geo.
             if end not in aacgm_sys:
-                lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), 
-                                                list(lont.flatten()), altitude,
+                lat, lon, _ = aacgm.aacgmConvArr(list(lat.flatten()), 
+                                                list(lon.flatten()), altitude,
                                                 date_time.year,1)
                 lon = np.array(lon).reshape(shape)
                 lat = np.array(lat).reshape(shape)
@@ -167,8 +185,8 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
         if end in aacgm_sys:
             # If it isn't in AACGM already it's in geo.
             if start == "geo":
-                lat, lon, _ = aacgm.aacgmConvArr(list(latt.flatten()), 
-                                                list(lont.flatten()), altitude,
+                lat, lon, _ = aacgm.aacgmConvArr(list(lat.flatten()), 
+                                                list(lon.flatten()), altitude,
                                                 date_time.year,0)
                 lon = np.array(lon).reshape(shape)
                 lat = np.array(lat).reshape(shape)
@@ -190,10 +208,10 @@ def coordConv(lon, lat, altitude, start, end, dateTime=None):
                                                    date_time.minute, 
                                                    date_time.second, 
                                                    el)
-                    if lon[num] > 12.:
-                        lon[num] -= 24.
                 # Convert hours to degrees.
                 lon *= 360./24.
+                # Convert from (0,360) to (-180,180).
+                lon[np.where(lon > 180.)] -= 360.
                 start = "mlt"
 
     # Now it should be in the end system.
@@ -246,83 +264,86 @@ if __name__ == "__main__":
     print "All of these results may have varying sigfigs."
     print "The expected values were found on a 32-bit system."
     print
+    print "Test of redirection function coordConv"
+    print coordConv(50.7, 34.5, 300., "geo", "geo")
+    print
     print "Single coord pair tests"
     print
     print "Test of list -> list"
     print "Expected:  ([50.700000000000003], [34.5])"
-    print "Result:    " + str(coordConv([50.7],[34.5],300.,'geo','geo'))
+    print "Result:    " + str(coord_conv([50.7],[34.5],300.,'geo','geo'))
     print
     print "Test of float -> float"
     print "Expected:  (50.700000000000003, 34.5)"
-    print "Result:    " + str(coordConv(50.7,34.5,300.,'geo','geo'))
+    print "Result:    " + str(coord_conv(50.7,34.5,300.,'geo','geo'))
     print
     print "Test of int -> float"
     print "Expected:  (50.0, 34.0)"
-    print "Result:    " + str(coordConv(50,34,300.,'geo','geo'))
+    print "Result:    " + str(coord_conv(50,34,300.,'geo','geo'))
     print
     print "Test of numpy array -> numpy array"
     print "Expected:  (array([ 50.7]), array([ 34.5]))"
-    print "Result:    " + str(coordConv(numpy.array([50.7]),numpy.array([34.5]),300.,'geo','geo'))
+    print "Result:    " + str(coord_conv(numpy.array([50.7]),numpy.array([34.5]),300.,'geo','geo'))
     print
     print "geo to geo, mag to mag, mlt to mlt, ashes to ashes"
     print "Expected:  (50.700000000000003, 34.5)"
-    print "Result:    " + str(coordConv(50.7,34.5,300.,'geo','geo'))
-    print "Result:    " + str(coordConv(50.7,34.5,300.,'mag','mag',dateTime=datetime(2013,7,23,12,6,34)))
-    print "Result:    " + str(coordConv(50.7,34.5,300.,'mlt','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result:    " + str(coord_conv(50.7,34.5,300.,'geo','geo'))
+    print "Result:    " + str(coord_conv(50.7,34.5,300.,'mag','mag',date_time=datetime(2013,7,23,12,6,34)))
+    print "Result:    " + str(coord_conv(50.7,34.5,300.,'mlt','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "geo to mag"
     print "Expected: (123.71642616363432, 31.582924632749929)"
-    print "Result:   " + str(coordConv(50.7,34.5,300.,'geo','mag',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result:   " + str(coord_conv(50.7,34.5,300.,'geo','mag',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "geo to mlt"
-    print "Expected: (229.34000961374397, 31.582924632749929)"
-    print "Result:   " + str(coordConv(50.7,34.5,300.,'geo','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Expected: (-130.65999038625603, 31.582924632749929)"
+    print "Result:   " + str(coord_conv(50.7,34.5,300.,'geo','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mag to geo"
     print "Expected: (50.563320914903102, 32.408924471374895)"
-    print "Result:   " + str(coordConv(123.53805352405843,29.419420613372086,300.,'mag','geo',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result:   " + str(coord_conv(123.53805352405843,29.419420613372086,300.,'mag','geo',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mlt to geo"
     print "Expected: (50.563320914903137, 32.408924471374895)"
-    print "Result:   " + str(coordConv(229.16163697416806,29.419420613372086,300.,'mlt','geo',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result:   " + str(coord_conv(229.16163697416806,29.419420613372086,300.,'mlt','geo',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mag to mlt"
-    print "Expected: (229.16163697416806, 29.419420613372086)"
-    print "Result:   " + str(coordConv(123.53805352405843,29.419420613372086,300.,'mag','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Expected: (-130.83836302583194, 29.419420613372086)"
+    print "Result:   " + str(coord_conv(123.53805352405843,29.419420613372086,300.,'mag','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mlt to mag"
     print "Expected: (123.53805352405843, 29.419420613372086)"
-    print "Result:   " + str(coordConv(229.16163697416806,29.419420613372086,300.,'mlt','mag',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result:   " + str(coord_conv(229.16163697416806,29.419420613372086,300.,'mlt','mag',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "Coord array tests"
     print
     print "geo to geo, mag to mag, mlt to mlt"
     print "Expected: ([50.700000000000003, 53.799999999999997], [34.5, 40.200000000000003])"
-    print "Result    " + str(coordConv([50.7,53.8],[34.5,40.2],300.,'geo','geo'))
-    print "Result    " + str(coordConv([50.7,53.8],[34.5,40.2],300.,'mag','mag',dateTime=datetime(2013,7,23,12,6,34)))
-    print "Result    " + str(coordConv([50.7,53.8],[34.5,40.2],300.,'mlt','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([50.7,53.8],[34.5,40.2],300.,'geo','geo'))
+    print "Result    " + str(coord_conv([50.7,53.8],[34.5,40.2],300.,'mag','mag',date_time=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([50.7,53.8],[34.5,40.2],300.,'mlt','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "geo to mag"
     print "Expected: ([123.65800072339718, 126.97463420949806], [30.892913121194589, 37.369211032553089])"
-    print "Result    " + str(coordConv([50.7,53.8],[34.5,40.2],[200.,300.],'geo','mag',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([50.7,53.8],[34.5,40.2],[200.,300.],'geo','mag',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "geo to mlt"
-    print "Expected: ([229.28158417350679, 232.59821765960771], [30.892913121194589, 37.369211032553089])"
-    print "Result    " + str(coordConv([50.7,53.8],[34.5,40.2],[200.,300.],'geo','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Expected: ([-130.71841582649319, -127.40178234039229], [30.892913121194589, 37.369211032553089])"
+    print "Result    " + str(coord_conv([50.7,53.8],[34.5,40.2],[200.,300.],'geo','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mag to geo"
     print "Expected: ([50.615511474515607, 53.648287906901672], [33.150771171950133, 38.637420715148586])"
-    print "Result    " + str(coordConv([123.53805352405843, 126.76454464467615],[29.419420613372086, 35.725172012254788],[200.,300.],'mag','geo',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([123.53805352405843, 126.76454464467615],[29.419420613372086, 35.725172012254788],[200.,300.],'mag','geo',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mlt to geo"
     print "Expected: ([50.563320914903137, 53.648287906901672], [32.408924471374895, 38.637420715148586])"
-    print "Result    " + str(coordConv([229.16163697416806, 232.38812809478577], [29.419420613372086, 35.725172012254788],300.,'mlt','geo',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([229.16163697416806, 232.38812809478577], [29.419420613372086, 35.725172012254788],300.,'mlt','geo',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mag to mlt"
-    print "Expected: ([229.16163697416806, 232.38812809478577], [29.419420613372086, 35.725172012254788])"
-    print "Result    " + str(coordConv([123.53805352405843, 126.76454464467615],[29.419420613372086, 35.725172012254788],300.,'mag','mlt',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Expected: ([-130.83836302583194, -127.61187190521423], [29.419420613372086, 35.725172012254788])"
+    print "Result    " + str(coord_conv([123.53805352405843, 126.76454464467615],[29.419420613372086, 35.725172012254788],300.,'mag','mlt',date_time=datetime(2013,7,23,12,6,34)))
     print
     print "mlt to mag"
     print "Expected: ([123.53805352405843, 126.76454464467615], [29.419420613372086, 35.725172012254788])"
-    print "Result    " + str(coordConv([229.16163697416806, 232.38812809478577], [29.419420613372086, 35.725172012254788],200.,'mlt','mag',dateTime=datetime(2013,7,23,12,6,34)))
+    print "Result    " + str(coord_conv([229.16163697416806, 232.38812809478577], [29.419420613372086, 35.725172012254788],200.,'mlt','mag',date_time=datetime(2013,7,23,12,6,34)))
     print
