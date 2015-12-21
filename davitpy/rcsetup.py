9e2b521eb177c44e53c7455e87ec775524c1a3e3
Ashton Seth Reimer <ashtonsethreimer@gmail.com>
2015-12-21 17:14:30 -0600

    Initial commit adding logging to davitpy. Added 'verbosity' parameter to rcParams.

--- davitpy/rcsetup.py
+++ davitpy/rcsetup.py
@@ -265,6 +265,7 @@ defaultParams = {
     'DAVIT_SD_LOCAL_FNAMEFMT':	['{date}.{hemi}.{ftype}', validate_string],
     'DAVIT_SD_REMOTE_TIMEINC':	['24', validate_string],
     'DAVIT_SD_LOCAL_TIMEINC':	['24', validate_string]
+    'verbosity':                ['helpful',validate_verbose]
 }
 
 
