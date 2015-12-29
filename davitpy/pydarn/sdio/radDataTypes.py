6695326fc834f89aeab0078aa5fe7f1d4182fd94
Ashton Seth Reimer <ashtonsethreimer@gmail.com>
2015-12-29 02:27:40 -0600

    Added missing import logging statement.

--- davitpy/pydarn/sdio/radDataTypes.py
+++ davitpy/pydarn/sdio/radDataTypes.py
@@ -33,6 +33,7 @@
 """
 
 import davitpy
+import logging
 from davitpy.utils import twoWayDict
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m', \
           'n','o','p','q','r','s','t','u','v','w','x','y','z']
