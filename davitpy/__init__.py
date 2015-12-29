eef05af7b0fa0ef111d5a638f4c37bc23136df90
ksterne <kevintyler@vt.edu>
2015-12-28 22:16:10 -0500

    Proper indentation and removed whitespaces

--- davitpy/__init__.py
+++ davitpy/__init__.py
@@ -582,19 +582,17 @@ rcParams = rc_params()
 #Change these try: excepts to trigger on proper exception, ie) except importError
 
 try: from davitpy import pydarn
-except Exception, e: 
-  print('problem importing pydarn: ', e)
+except Exception, e:
+    print('problem importing pydarn: ', e)
 
-try: from davitpy import gme 
-except Exception, e: 
-  print('problem importing gme: ', e)
+try: from davitpy import gme
+except Exception, e:
+    print('problem importing gme: ', e)
 
-try: from davitpy import utils 
-except Exception, e: 
-  print('problem importing utils: ', e)
+try: from davitpy import utils
+except Exception, e:
+    print('problem importing utils: ', e)
 
-try: from davitpy import models 
-except Exception, e: 
-  print('problem importing models: ', e)
-
-
+try: from davitpy import models
+except Exception, e:
+    print('problem importing models: ', e)
