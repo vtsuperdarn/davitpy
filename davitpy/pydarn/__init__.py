abd5a5136bf5c3b8bc5c08fc94a5f912ff7383f1
Ashton Seth Reimer <ashtonsethreimer@gmail.com>
2015-12-30 20:33:32 -0600

    Converted pydarn's init.py to numpy docstring style and to use logging.

--- davitpy/pydarn/__init__.py
+++ davitpy/pydarn/__init__.py
@@ -1,35 +1,45 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
 """
-*********************
-**Module**: pydarn
-*********************
+pydarn
+------
+
 Module for everything SuperDARN
 
-**Modules**:
-    * :mod:`pydarn.sdio`: superdarn data I/O
-    * :mod:`pydarn.radar`: radar structures and utilities
-    * :mod:`pydarn.plotting`: radar plotting utilities
-    * :mod:`pydarn.proc`: adanced data processing utilities
+Modules
+-------------------------------------------
+sdio     superdarn data I/O
+radar    radar structures and utilities
+plotting radar plotting utilities
+proc     adanced data processing utilities
+-------- ----------------------------------
+
 """
 
-try: import dmapio 
-except Exception, e: 
-  print 'problem importing dmapio: ', e
+import logging
 
-try: import radar 
-except Exception, e: 
-  print 'problem importing radar: ', e
+try:
+	import dmapio
+except Exception, e:
+    logging.exception('problem importing dmapio: ' + e)
 
-try: import sdio 
-except Exception, e: 
-  print 'problem importing sdio: ', e
+try:
+	import radar
+except Exception, e:
+    logging.exception('problem importing radar: ' + e)
 
-try: import plotting 
-except Exception, e: 
-  print 'problem importing plotting: ', e
+try:
+	import sdio
+except Exception, e:
+    logging.exception('problem importing sdio: ' + e)
 
-try: import proc 
-except Exception, e: 
-  print 'problem importing proc: ', e
+try:
+	import plotting
+except Exception, e:
+    logging.exception('problem importing plotting: ' + e)
 
+try:
+	import proc
+except Exception, e:
+    logging.exception('problem importing proc: ' + e)
