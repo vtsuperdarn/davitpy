d083896017d9fc713e6aeb569ae2ba0b445b0e3f
ksterne <kevintyler@vt.edu>
2016-01-27 21:42:18 -0500

    Numpy docstring added to dmapio init

--- davitpy/pydarn/dmapio/__init__.py
+++ davitpy/pydarn/dmapio/__init__.py
@@ -14,7 +14,13 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+dmapio
+------
 
+Module for interfacing with dmapio c code
+
+"""
 import logging
 
 try:
