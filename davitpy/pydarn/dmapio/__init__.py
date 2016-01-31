51b175f5a94a012e59d2b55b5b49cf22f1fc6480
ksterne <kevintyler@vt.edu>
2016-01-31 10:57:32 -0500

    Making __init__.py consistent

--- davitpy/pydarn/dmapio/__init__.py
+++ davitpy/pydarn/dmapio/__init__.py
@@ -14,7 +14,9 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""dmapio module
+"""
+dmapio
+------
 
 Module for interfacing with dmapio c code
 
