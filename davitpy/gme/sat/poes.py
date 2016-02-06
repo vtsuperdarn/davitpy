c16d3e868322cc675108d66a8146f41d63bd2ac2
ksterne <kevintyler@vt.edu>
2016-02-06 10:29:34 -0500

    Converting more docstring poes.py

--- davitpy/gme/sat/poes.py
+++ davitpy/gme/sat/poes.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """poes module
@@ -40,80 +40,140 @@ import logging
 
 
 class poesRec(gmeData):
-  """a class to represent a record of poes data.  Extends :class:`gmeBase.gmeData`.  Insight on the class members can be obtained from `the NOAA NGDC site <ftp://satdat.ngdc.noaa.gov/sem/poes/data/readme.txt>`_.  Note that Poes data is available from 1998-present day (or whatever the latest NOAA has uploaded is).  **The data are the 16-second averages**
-  
-  **Members**: 
-    * **time** (`datetime <http://tinyurl.com/bl352yx>`_): an object identifying which time these data are for
-    * **info** (str): information about where the data come from.  *Please be courteous and give credit to data providers when credit is due.*
-    * **dataSet** (str): the name of the data set
-    * **satnum** (ind): the noaa satellite number
-    * **sslat** (float): Geographic Latitude of sub-satellite point, degrees
-    * **sslon** (float): Geographic Longitude of sub-satellite point, degrees
-    * **folat** (float): Geographic Latitude of foot-of-field-line, degrees
-    * **folon** (float): Geographic Longitude of foot-of-field-line, degrees
-    * **lval** (float): L-value
-    * **mlt** (float): Magnetic local time of foot-of-field-line, degrees
-    * **pas0** (float): MEPED-0 pitch angle at satellite, degrees
-    * **pas90** (float): MEPED-90 pitch angle at satellite, degrees
-    * **mep0e1** (float): MEPED-0 > 30 keV electrons, counts/sec
-    * **mep0e2** (float): MEPED-0 > 100 keV electrons, counts/sec
-    * **mep0e3** (float): MEPED-0 > 300 keV electrons, counts/sec
-    * **mep0p1** (float):MEPED-0 30 keV to 80 keV protons, counts/sec
-    * **mep0p2** (float): MEPED-0 80 keV to 240 keV protons, counts/sec
-    * **mep0p3** (float): 240 kev to 800 keV protons, counts/sec
-    * **mep0p4** (float): MEPED-0 800 keV to 2500 keV protons, counts/sec
-    * **mep0p5** (float): MEPED-0 2500 keV to 6900 keV protons, counts/sec
-    * **mep0p6** (float): MEPED-0 > 6900 keV protons, counts/sec,
-    * **mep90e1** (float): MEPED-90 > 30 keV electrons, counts/sec,
-    * **mep90e2** (float): MEPED-90 > 100 keV electrons, counts/sec
-    * **mep90e3** (float): MEPED-90 > 300 keV electrons, counts/sec
-    * **mep90p1** (float): MEPED-90 30 keV to 80 keV protons, counts/sec
-    * **mep90p2** (float): MEPED-90 80 keV to 240 keV protons, counts/sec
-    * **mep90p3** (float): MEPED-90 240 kev to 800 keV protons, counts/sec,
-    * **mep90p4** (float): MEPED-90 800 keV to 2500 keV protons, counts/sec
-    * **mep90p5** (float): MEPED-90 2500 keV to 6900 keV protons, counts/sec
-    * **mep90p6** (float):MEPED-90 > 6900 keV protons, counts/sec
-    * **mepomp6** (float): MEPED omni-directional > 16 MeV protons, counts/sec
-    * **mepomp7** (float): MEPED omni-directional > 36 Mev protons, counts/sec
-    * **mepomp8** (float): MEPED omni-directional > 70 MeV protons, counts/sec
-    * **mepomp9** (float): MEPED omni-directional >= 140 MeV protons
-    * **ted** (float): TED, Total Energy Detector Average, ergs/cm2/sec
-    * **echar** (float): TED characteristic energy of electrons, eV
-    * **pchar** (float): TED characteristic energy of protons, eV
-    * **econtr** (float): TED electron contribution, Electron Energy/Total Energy
-    
-    
-  .. note::
-    If any of the members have a value of None, this means that they could not be read for that specific time
+    """a class to represent a record of poes data.
+
+    Extends class gmeBase.gmeData.  Insight on the class members can
+    be obtained from the NOAA NGDC site
+    <ftp://satdat.ngdc.noaa.gov/sem/poes/data/readme.txt>.  Note that
+    Poes data is available from 1998-present day (or whatever the
+    latest NOAA has uploaded is).  **The data are the 16-second
+    averages**
+
+    Attributes
+    ----------
+    time : datetime
+        an object identifying which time these data are for
+    info : str
+        information about where the data come from.  *Please be
+        courteous and give credit to data providers when credit is due.*
+    dataSet : str
+        the name of the data set
+    satnum : ind
+        the noaa satellite number
+    sslat : float
+        Geographic Latitude of sub-satellite point, degrees
+    sslon : float
+        Geographic Longitude of sub-satellite point, degrees
+    folat : float
+        Geographic Latitude of foot-of-field-line, degrees
+    folon : float
+        Geographic Longitude of foot-of-field-line, degrees
+    lval : float
+        L-value
+    mlt : float
+        Magnetic local time of foot-of-field-line, degrees
+    pas0 : float
+        MEPED-0 pitch angle at satellite, degrees
+    pas90 : float
+        MEPED-90 pitch angle at satellite, degrees
+    mep0e1 : float
+        MEPED-0 > 30 keV electrons, counts/sec
+    mep0e2 : float
+        MEPED-0 > 100 keV electrons, counts/sec
+    mep0e3 : float
+        MEPED-0 > 300 keV electrons, counts/sec
+    mep0p1 : float
+        MEPED-0 30 keV to 80 keV protons, counts/sec
+    mep0p2 : float
+        MEPED-0 80 keV to 240 keV protons, counts/sec
+    mep0p3 : float
+        240 kev to 800 keV protons, counts/sec
+    mep0p4 : float
+        MEPED-0 800 keV to 2500 keV protons, counts/sec
+    mep0p5 : float
+        MEPED-0 2500 keV to 6900 keV protons, counts/sec
+    mep0p6 : float
+        MEPED-0 > 6900 keV protons, counts/sec,
+    mep90e1 : float
+        MEPED-90 > 30 keV electrons, counts/sec,
+    mep90e2 : float
+        MEPED-90 > 100 keV electrons, counts/sec
+    mep90e3 : float
+        MEPED-90 > 300 keV electrons, counts/sec
+    mep90p1 : float
+        MEPED-90 30 keV to 80 keV protons, counts/sec
+    mep90p2 : float
+        MEPED-90 80 keV to 240 keV protons, counts/sec
+    mep90p3 : float
+        MEPED-90 240 kev to 800 keV protons, counts/sec,
+    mep90p4 : float
+        MEPED-90 800 keV to 2500 keV protons, counts/sec
+    mep90p5 : float
+        MEPED-90 2500 keV to 6900 keV protons, counts/sec
+    mep90p6 : float
+        MEPED-90 > 6900 keV protons, counts/sec
+    mepomp6 : float
+        MEPED omni-directional > 16 MeV protons, counts/sec
+    mepomp7 : float
+        MEPED omni-directional > 36 Mev protons, counts/sec
+    mepomp8 : float
+        MEPED omni-directional > 70 MeV protons, counts/sec
+    mepomp9 : float
+        MEPED omni-directional >= 140 MeV protons
+    ted : float
+        TED, Total Energy Detector Average, ergs/cm2/sec
+    echar : float
+        TED characteristic energy of electrons, eV
+    pchar : float
+        TED characteristic energy of protons, eV
+    econtr : float
+        TED electron contribution, Electron Energy/Total Energy
+
+    Notes
+    -----
+    If any of the members have a value of None, this means that they could not
+    be read for that specific time
    
-  **Methods**:
-    * :func:`parseFtp`
-  **Example**:
-    ::
+    Methods
+    -------
+    parseFtp
+
+    Example
+    -------
+        emptyPoesObj = gme.sat.poesRec()
     
-      emptyPoesObj = gme.sat.poesRec()
-    
-  written by AJ, 20130131
-  """
-  
-  def parseFtp(self,line, header):
-    """This method is used to convert a line of poes data read from the NOAA NGDC FTP site into a :class:`poesRec` object.
-    
-    .. note::
-      In general, users will not need to worry about this.
-    
-    **Belongs to**: :class:`poesRec`
-    
-    **Args**: 
-      * **line** (str): the ASCII line from the FTP server
-    **Returns**:
-      * Nothing.
-    **Example**:
-      ::
-      
-        myPoesObj.parseFtp(ftpLine)
-      
     written by AJ, 20130131
+
+    """
+
+
+  def parseFtp(self,line, header):
+    """This method is used to convert a line of poes data read from
+    the NOAA NGDC FTP site into a :class:`poesRec` object.
+
+    Parameters
+    ----------
+    line : str
+        the ASCII line from the FTP server
+    header :
+
+
+    Returns
+    -------
+    Nothing.
+
+    Notes
+    -----
+    In general, users will not need to worry about this.
+
+    Belongs to class poesRec
+
+    Example
+    -------
+        myPoesObj.parseFtp(ftpLine)
+
+    written by AJ, 20130131
+
     """
     import datetime as dt
     
@@ -131,30 +191,43 @@ class poesRec(gmeData):
         logging.exception('problem setting attribute' + key)
       #check for a good value
       if(float(cols[ind]) != -999.): setattr(self,key,float(cols[ind]))
+
   
   def __init__(self, ftpLine=None, dbDict=None, satnum=None, header=None):
-    """the intialization fucntion for a :class:`omniRec` object.  
-    
-    .. note::
-      In general, users will not need to worry about this.
-    
-    **Belongs to**: :class:`omniRec`
-    
-    **Args**: 
-      * [**ftpLine**] (str): an ASCII line from the FTP server. if this is provided, the object is initialized from it.  header must be provided in conjunction with this.  default=None
-      * [**header**] (str): the header from the ASCII FTP file.  default=None
-      * [**dbDict**] (dict): a dictionary read from the mongodb.  if this is provided, the object is initialized from it.  default = None
-      * [**satnum**] (int): the satellite nuber.  default=None
-    **Returns**:
-      * Nothing.
-    **Example**:
-      ::
-      
+    """the intialization fucntion for a class omniRec object.  
+
+    Parameters
+    ----------
+    ftpLine : Optional[str]
+        an ASCII line from the FTP server. if this is provided, the
+        object is initialized from it.  header must be provided in
+        conjunction with this.  default=None
+    header : Optional[str]
+        the header from the ASCII FTP file.  default=None
+    dbDict : Optional[dict]
+        a dictionary read from the mongodb.  if this is provided,
+        the object is initialized from it.  default = None
+    satnum : Optional[int]
+        the satellite nuber.  default=None
+
+    Returns
+    -------
+    Nothing.
+
+    Notes
+    -----
+    In general, users will not need to worry about this.
+
+    Belongs to class omniRec
+
+    Example
+    -------
         myPoesObj = poesRec(ftpLine=aftpLine)
       
     written by AJ, 20130131
+
     """
-    #note about where data came from
+    # note about where data came from
     self.dataSet = 'Poes'
     self.info = 'These data were downloaded from NASA SPDF.  *Please be courteous and give credit to data providers when credit is due.*'
     self.satnum = satnum
@@ -198,34 +271,61 @@ class poesRec(gmeData):
     if(dbDict != None): self.parseDb(dbDict)
     
 def readPoes(sTime,eTime=None,satnum=None,folat=None,folon=None,ted=None,echar=None,pchar=None):
-  """This function reads poes data.  First, it will try to get it from the mongodb, and if it can't find it, it will look on the NOAA NGDC FTP server using :func:`readPoesFtp`.  The data are 16-second averages
+  """This function reads poes data.
 
-  **Args**: 
-    * **sTime** (`datetime <http://tinyurl.com/bl352yx>`_ or None): the earliest time you want data for
-    * [**eTime**] (`datetime <http://tinyurl.com/bl352yx>`_ or None): the latest time you want data for.  if this is None, end Time will be 1 day after sTime.  default = None
-    * [**satnum**] (int): the satellite you want data for.  eg 17 for noaa17.  if this is None, data for all satellites will be returned.  default = None
-    * [**satnum**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bx values in the range [a,b] will be returned.  default = None
-    * [**folat**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bx values in the range [a,b] will be returned.  default = None
-    * [**folon**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bye values in the range [a,b] will be returned.  default = None
-    * [**ted**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bze values in the range [a,b] will be returned.  default = None
-    * [**echar**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bym values in the range [a,b] will be returned.  default = None
-    * [**pchar**] (list or None): if this is not None, it must be a 2-element list of numbers, [a,b].  In this case, only data with bzm values in the range [a,b] will be returned.  default = None
+  First, it will try to get it from the mongodb, and if it can't find it, it will
+  look on the NOAA NGDC FTP server using :func:`readPoesFtp`.  The data are
+  16-second averages
+
+  Parameters
+  ----------
+  sTime : datetime or None
+    the earliest time you want data for
+  eTime : Optional[datetime or None]
+    the latest time you want data for.  if this is None, end Time will be 1
+    day after sTime.  default = None
+  satnum : Optional[int]
+    the satellite you want data for.  eg 17 for noaa17.  if this is None, data
+    for all satellites will be returned.  default = None
+  folat : Optional[list or None]
+    if this is not None, it must be a 2-element list of numbers, [a,b].  In
+    this case, only data with bx values in the range [a,b] will be returned.
+    default = None
+  folon : Optional[list or None]
+    if this is not None, it must be a 2-element list of numbers, [a,b].  In
+    this case, only data with bye values in the range [a,b] will be returned.
+    default = None
+  ted : Optional[list or None]
+    if this is not None, it must be a 2-element list of numbers, [a,b].  In
+    this case, only data with bze values in the range [a,b] will be returned.
+    default = None
+  echar : Optional[list or None]
+    if this is not None, it must be a 2-element list of numbers, [a,b].  In
+    this case, only data with bym values in the range [a,b] will be returned.
+    default = None
+  pchar : Optional[list or None]
+    if this is not None, it must be a 2-element list of numbers, [a,b].  In
+    this case, only data with bzm values in the range [a,b] will be returned.
+    default = None
     
-  **Returns**:
-    * **poesList** (list or None): if data is found, a list of :class:`poesRec` objects matching the input parameters is returned.  If no data is found, None is returned.
-  **Example**:
-    ::
-    
+  Returns
+  -------
+  poesList : list or None
+    if data is found, a list of class poesRec objects matching the input
+    parameters is returned.  If no data is found, None is returned.
+
+  Example
+  -------
       import datetime as dt
       poesList = gme.sat.readPoes(sTime=dt.datetime(2011,1,1),eTime=dt.datetime(2011,6,1),folat=[60,80])
     
   written by AJ, 20130131
+
   """
-  
   import datetime as dt
   import davitpy.pydarn.sdio.dbUtils as db
-  
-  #check all the inputs for validity
+
+  # check all the inputs for validity
   assert(isinstance(sTime,dt.datetime)), logging.error(
     'sTime must be a datetime object')
   assert(eTime == None or isinstance(eTime,dt.datetime)), logging.error(
@@ -237,10 +337,10 @@ def readPoes(sTime,eTime=None,satnum=None,folat=None,folon=None,ted=None,echar=N
       isinstance(var[name][0],(int,float)) and \
       isinstance(var[name][1],(int,float)))), logging.error(
       name + ' must None or a list of 2 numbers')
-    
+
   if(eTime == None): eTime = sTime+dt.timedelta(days=1)
   qryList = []
-  #if arguments are provided, query for those
+  # if arguments are provided, query for those
   qryList.append({'time':{'$gte':sTime}})
   if(eTime != None): qryList.append({'time':{'$lte':eTime}})
   if(satnum != None): qryList.append({'satnum':satnum})
@@ -249,13 +349,13 @@ def readPoes(sTime,eTime=None,satnum=None,folat=None,folon=None,ted=None,echar=N
     if(var[name] != None): 
       qryList.append({name:{'$gte':min(var[name])}})
       qryList.append({name:{'$lte':max(var[name])}})
-      
-  #construct the final query definition
+
+  # construct the final query definition
   qryDict = {'$and': qryList}
-  #connect to the database
+  # connect to the database
   poesData = db.getDataConn(dbName='gme',collName='poes')
-  
-  #do the query
+
+  # do the query
   if(qryList != []): qry = poesData.find(qryDict)
   else: qry = poesData.find()
   if(qry.count() > 0):
@@ -264,22 +364,23 @@ def readPoes(sTime,eTime=None,satnum=None,folat=None,folon=None,ted=None,echar=N
       poesList.append(poesRec(dbDict=rec))
     logging.info('nreturning a list with '+ len(poesList) + ' records of poes data')
     return poesList
-  #if we didn't find anything on the mongodb
+  # if we didn't find anything on the mongodb
   else:
     logging.info('could not find requested data in the mongodb')
     return None
-    #print 'we will look on the ftp server, but your conditions will be (mostly) ignored'
+    # print 'we will look on the ftp server, but your conditions will be (mostly) ignored'
     
-    ##read from ftp server
-    #poesList = readPoesFtp(sTime, eTime)
+    ## read from ftp server
+    # poesList = readPoesFtp(sTime, eTime)
     
-    #if(poesList != None):
-      #print '\nreturning a list with',len(poesList),'recs of poes data'
-      #return poesList
-    #else:
-      #print '\n no data found on FTP server, returning None...'
-      #return None
-      
+    # if(poesList != None):
+      # print '\nreturning a list with',len(poesList),'recs of poes data'
+      # return poesList
+    # else:
+      # print '\n no data found on FTP server, returning None...'
+      # return None
+
+
 def readPoesFtp(sTime,eTime=None):
   """This function reads poes data from the NOAA NGDC server via anonymous FTP connection.
   
