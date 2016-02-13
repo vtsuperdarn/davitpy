6b8fd36d73c6fb0861af8cb8f2f469bed6687906
ksterne <kevintyler@vt.edu>
2016-02-13 15:17:57 -0500

    Numpy docstring gme/ind/init

--- davitpy/gme/ind/__init__.py
+++ davitpy/gme/ind/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
 # 
@@ -17,13 +18,19 @@
 # ind module __init__.py
 """
 ind
-****
+---
+
 This subpackage contains various fucntions to read and write Geomagnetic Indices
 
-This includes the following modules:
+Modules
+----------------------------
+ae      AE data
+dst     Dst data
+kp      Kp data
+omni    OMNI data (from ACE)
+symasy  SYM/ASY data
+----------------------------
 
-	* **kp**
-	* **omni**
 """
 import logging
 
