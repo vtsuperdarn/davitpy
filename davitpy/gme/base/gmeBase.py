311afe166184ee683c610c7b9fdf39ed850ffbc0
ksterne <kevintyler@vt.edu>
2016-02-17 22:10:49 -0500

    Numpy docstring conversion

--- davitpy/gme/base/gmeBase.py
+++ davitpy/gme/base/gmeBase.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
 # 
@@ -13,60 +14,75 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""gmeBase module
 
+A base class for gme data.  Allows definition of common routines
 
-"""
-.. module:: gmeBase
-   :synopsis: A base class for gme data.  Allows definition of common routines
+Classes
+-----------------------------
+gmeData     a gme data record
+-----------------------------
 
-.. moduleauthor:: AJ, 20130129
+Module author:: AJ, 20130129
 
-*****************************
-**Module**: gmeBase
-*****************************
-**Classes**:
-	* :class:`gmeData`
 """
 import logging
 
 class gmeData:
 	"""a class to represent a a record of gme data.  Other classes will extend this class
 		
-	**Members**: 
-		* **time** (`datetime <http://tinyurl.com/bl352yx>`_): an object identifying which time these data are for
-		* **dataSet** (str): a string indicating the dataset
-		* **info** (str): information about where the data come from.  *Please be courteous and give credit to data providers when credit is due.*
-	.. note::
-		If any of the members have a value of None, this means that they could not be read for that specific time
+	Attributes
+    ----------
+	time : datetime
+        an object identifying which time these data are for
+	dataSet : str
+        a string indicating the dataset
+	info : str
+        information about where the data come from.  *Please be courteous and give
+        credit to data providers when credit is due.*
+
+	Methods
+    -------
+	parseDb
+	toDbDict
+
+	Notes
+    -----
+	If any of the members have a value of None, this means that they could not
+    be read for that specific time
    
-	**Methods**:
-		* :func:`parseDb`
-		* :func:`toDbDict`
-	**Example**:
-		::
-		
+	Example
+    -------
 			emptyObj = gme.base.gmeData()
 		
 	written by AJ, 20130131
+
 	"""
 	def parseDb(self,dbDict):
-		"""This method is used to parse a dictionary of gme data from the mongodb into a :class:`gmeData` object.  
+		"""This method is used to parse a dictionary of gme data from the mongodb
+        into a :class:`gmeData` object.  
 		
-		.. note:: 
-			In general, users will not need to use this.
+		Parameters
+        ----------
+		dbDict : dict
+            the dictionary from the mongodb
+
+		Returns
+        -------
+		Nothing
+
+		Notes
+        -----
+		In general, users will not need to use this.
 		
-		**Belongs to**: :class:`gmeData`
+		Belongs to class gmeData
 		
-		**Args**: 
-			* **dbDict** (dict): the dictionary from the mongodb
-		**Returns**:
-			* Nothing.
-		**Example**:
-			::
-			
+		Example
+        -------
 				myObj.parseDb(mongoDbDict)
 			
 		written by AJ, 20130129
+
 		"""
 		#iterate over the mongo dict
 		for attr, val in dbDict.iteritems():
@@ -86,25 +102,33 @@ class gmeData:
 				except Exception,e:
 					logging.exception(e)
 					logging.exception('problem assigning ' + attr)
+
 					
 	def toDbDict(self):
-		"""This method is used to convert a :class:`gmeData` object into a mongodb data dictionary.
-		
-		.. note::
-			In general, users will not need to worry about this
-		
-		**Belongs to**: :class:`gmeData`
-		
-		**Args**: 
-			* Nothing.
-		**Returns**:
-			* **dbDict** (dict): a dictionary in the correct format for writing to the mongodb
-		**Example**:
-			::
-			
+		"""This method is used to convert a :class:`gmeData` object into a
+        mongodb data dictionary.
+
+		Parameters
+        ----------
+		Nothing
+
+		Returns
+        -------
+		dbDict : dict
+            a dictionary in the correct format for writing to the mongodb
+
+		Notes
+        -----
+		In general, users will not need to worry about this
+
+		Belongs to class gmeData
+
+		Example
+        -------
 				mongoDbDict = myObj.todbDict()
-			
+
 		written by AJ, 20130129
+
 		"""
 		#initialize a new dictionary
 		dbDict = {}
@@ -121,12 +145,14 @@ class gmeData:
 					dbDict['kp'].append(num)
 			else: dbDict[attr] = val
 		return dbDict
+
 		
 	def __repr__(self):
 		myStr = self.dataSet+' record FROM: '+str(self.time)+'\n'
 		for key,var in self.__dict__.iteritems():
 			myStr += key+' = '+str(var)+'\n'
 		return myStr
+
 	
 	def __init__(self):
 		self.time = None
