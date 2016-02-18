3866cf9f33e7ad68cae1748fb1b9883cfa77cd86
ksterne <kevintyler@vt.edu>
2016-02-18 07:10:32 -0500

    Tabs to spaces

--- davitpy/gme/base/fillGmedb.py
+++ davitpy/gme/base/fillGmedb.py
@@ -35,88 +35,88 @@ def fillGmeDb(time='recent'):
     -------
 
     """
-	from davitpy import gme
-        from davitpy import rcParams
-        import os
-	import davitpy.pydarn.sdio.dbUtils as dbu
-	from multiprocessing import Process
-	import datetime as dt
-	now = dt.datetime.now()
-	
-	if(time == 'recent'):
-		sYear = dt.datetime.now().year-5
-		#fill the omni database
-		p0 = Process(target=gme.ind.omni.mapOmniMongo, args=(sYear,now.year,1))
-		#fill the omni database
-		p1 = Process(target=gme.ind.omni.mapOmniMongo, args=(sYear,now.year))
-		#fill the poes database
-		p2 = Process(target=gme.sat.poes.mapPoesMongo, args=(sYear,now.year))
-		#fill the kp database
-		p3 = Process(target=gme.ind.kp.mapKpMongo, args=(sYear,now.year))
-		#fill the kp database
-		p4 = Process(target=gme.ind.dst.mapDstMongo, args=(sYear,now.year))
-		#fill the kp database
-		p5 = Process(target=gme.ind.ae.mapAeMongo, args=(sYear,now.year))
-		#fill the kp database
-		p6 = Process(target=gme.ind.symasy.mapSymAsyMongo, args=(sYear,now.year))
-	else:
-		db = dbu.getDbConn(username=rcParams['DBWRITEUSER'],password=rcParams['DBWRITEPASS'],dbName='gme')
-		db.command('repairDatabase')
-		#fill the omni database
-		p0 = Process(target=gme.ind.omni.mapOmniMongo, args=(1995,now.year,1))
-		#fill the omni database
-		p1 = Process(target=gme.ind.omni.mapOmniMongo, args=(1995,now.year))
-		#fill the poes database
-		p2 = Process(target=gme.sat.poes.mapPoesMongo, args=(1998,now.year))
-		#fill the kp database
-		p3 = Process(target=gme.ind.kp.mapKpMongo, args=(1980,now.year))
-		#fill the kp database
-		p4 = Process(target=gme.ind.dst.mapDstMongo, args=(1980,now.year))
-		#fill the kp database
-		p5 = Process(target=gme.ind.ae.mapAeMongo, args=(1980,now.year))
-		#fill the kp database
-		p6 = Process(target=gme.ind.symasy.mapSymAsyMongo, args=(1980,now.year))
-		
-	try: p0.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exeption('problem filling Omni db')
-		
-	try: p1.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling Omni db')
-		
-	try: p2.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling Poes db')
-		
-	try: p3.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling Kp db')
-		
-	try: p4.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling Dst db')
-		
-	try: p5.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling AE db')
-		
-	try: p6.start()
-	except Exception,e:
-		logging.exception(e)
-		logging.exception('problem filling AE db')
-		
-		
-	p0.join()
-	p1.join()
-	p2.join()
-	p3.join()
-	p4.join()
-	p5.join()
-	p6.join()
+    from davitpy import gme
+    from davitpy import rcParams
+    import os
+    import davitpy.pydarn.sdio.dbUtils as dbu
+    from multiprocessing import Process
+    import datetime as dt
+    now = dt.datetime.now()
+
+    if(time == 'recent'):
+        sYear = dt.datetime.now().year-5
+        #fill the omni database
+        p0 = Process(target=gme.ind.omni.mapOmniMongo, args=(sYear,now.year,1))
+        #fill the omni database
+        p1 = Process(target=gme.ind.omni.mapOmniMongo, args=(sYear,now.year))
+        #fill the poes database
+        p2 = Process(target=gme.sat.poes.mapPoesMongo, args=(sYear,now.year))
+        #fill the kp database
+        p3 = Process(target=gme.ind.kp.mapKpMongo, args=(sYear,now.year))
+        #fill the kp database
+        p4 = Process(target=gme.ind.dst.mapDstMongo, args=(sYear,now.year))
+        #fill the kp database
+        p5 = Process(target=gme.ind.ae.mapAeMongo, args=(sYear,now.year))
+        #fill the kp database
+        p6 = Process(target=gme.ind.symasy.mapSymAsyMongo, args=(sYear,now.year))
+    else:
+        db = dbu.getDbConn(username=rcParams['DBWRITEUSER'],password=rcParams['DBWRITEPASS'],dbName='gme')
+        db.command('repairDatabase')
+        #fill the omni database
+        p0 = Process(target=gme.ind.omni.mapOmniMongo, args=(1995,now.year,1))
+        #fill the omni database
+        p1 = Process(target=gme.ind.omni.mapOmniMongo, args=(1995,now.year))
+        #fill the poes database
+        p2 = Process(target=gme.sat.poes.mapPoesMongo, args=(1998,now.year))
+        #fill the kp database
+        p3 = Process(target=gme.ind.kp.mapKpMongo, args=(1980,now.year))
+        #fill the kp database
+        p4 = Process(target=gme.ind.dst.mapDstMongo, args=(1980,now.year))
+        #fill the kp database
+        p5 = Process(target=gme.ind.ae.mapAeMongo, args=(1980,now.year))
+        #fill the kp database
+        p6 = Process(target=gme.ind.symasy.mapSymAsyMongo, args=(1980,now.year))
+
+    try: p0.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exeption('problem filling Omni db')
+
+    try: p1.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling Omni db')
+
+    try: p2.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling Poes db')
+
+    try: p3.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling Kp db')
+
+    try: p4.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling Dst db')
+
+    try: p5.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling AE db')
+
+    try: p6.start()
+    except Exception,e:
+        logging.exception(e)
+        logging.exception('problem filling AE db')
+
+
+    p0.join()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
--- davitpy/gme/base/gmeBase.py
+++ davitpy/gme/base/gmeBase.py
@@ -58,7 +58,7 @@ class gmeData:
     written by AJ, 20130131
 
     """
-	def parseDb(self,dbDict):
+    def parseDb(self,dbDict):
         """This method is used to parse a dictionary of gme data from the mongodb
         into a :class:`gmeData` object.  
 
@@ -84,27 +84,27 @@ class gmeData:
         written by AJ, 20130129
 
         """
-		#iterate over the mongo dict
-		for attr, val in dbDict.iteritems():
-			#check for mongo _id attribute
-			if(attr == '_id'): pass
-			elif(attr == 'kp'):
-				for i in range(len(dbDict['kp'])):
-					num = str(int(dbDict['kp'][i]))
-					mod = dbDict['kp'][i] - int(dbDict['kp'][i])
-					if(mod == .3): mod = '-'
-					elif(mod == .7): mod = '+'
-					else: mod = ''
-					self.kp.append(num+mod) 
-			else:
-				#assign the value to our object
-				try: setattr(self,attr,val)
-				except Exception,e:
-					logging.exception(e)
-					logging.exception('problem assigning ' + attr)
+        #iterate over the mongo dict
+        for attr, val in dbDict.iteritems():
+            #check for mongo _id attribute
+            if(attr == '_id'): pass
+            elif(attr == 'kp'):
+                for i in range(len(dbDict['kp'])):
+                    num = str(int(dbDict['kp'][i]))
+                    mod = dbDict['kp'][i] - int(dbDict['kp'][i])
+                    if(mod == .3): mod = '-'
+                    elif(mod == .7): mod = '+'
+                    else: mod = ''
+                    self.kp.append(num+mod) 
+            else:
+                #assign the value to our object
+                try: setattr(self,attr,val)
+                except Exception,e:
+                    logging.exception(e)
+                    logging.exception('problem assigning ' + attr)
 
-					
-	def toDbDict(self):
+
+    def toDbDict(self):
         """This method is used to convert a :class:`gmeData` object into a
         mongodb data dictionary.
 
@@ -130,31 +130,31 @@ class gmeData:
         written by AJ, 20130129
 
         """
-		#initialize a new dictionary
-		dbDict = {}
-		#create dictionary entries for all out our attributes
-		for attr, val in self.__dict__.iteritems():
-			if(attr == 'kp'):
-				dbDict['kp'] = []
-				for i in range(len(self.kp)):
-					num = int(self.kp[i][0:1])
-					mod = self.kp[i][1:2]
-					if(mod == '+'): num += .7
-					elif(mod == '-'): num += .3
-					else: num += .5
-					dbDict['kp'].append(num)
-			else: dbDict[attr] = val
-		return dbDict
+        #initialize a new dictionary
+        dbDict = {}
+        #create dictionary entries for all out our attributes
+        for attr, val in self.__dict__.iteritems():
+            if(attr == 'kp'):
+                dbDict['kp'] = []
+                for i in range(len(self.kp)):
+                    num = int(self.kp[i][0:1])
+                    mod = self.kp[i][1:2]
+                    if(mod == '+'): num += .7
+                    elif(mod == '-'): num += .3
+                    else: num += .5
+                    dbDict['kp'].append(num)
+            else: dbDict[attr] = val
+        return dbDict
 
-		
-	def __repr__(self):
-		myStr = self.dataSet+' record FROM: '+str(self.time)+'\n'
-		for key,var in self.__dict__.iteritems():
-			myStr += key+' = '+str(var)+'\n'
-		return myStr
 
-	
-	def __init__(self):
-		self.time = None
-		self.dataSet = None
-		self.info = None
+    def __repr__(self):
+        myStr = self.dataSet+' record FROM: '+str(self.time)+'\n'
+        for key,var in self.__dict__.iteritems():
+            myStr += key+' = '+str(var)+'\n'
+        return myStr
+
+    
+    def __init__(self):
+        self.time = None
+        self.dataSet = None
+        self.info = None
