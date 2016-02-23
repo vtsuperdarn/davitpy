15827810e65a2b11af8d6aa5653ff4b6da94b3ad
ksterne <kevintyler@vt.edu>
2016-02-22 21:38:49 -0500

    Numpy docstring conversion

--- davitpy/models/hwm/__init__.py
+++ davitpy/models/hwm/__init__.py
@@ -1,19 +1,44 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
-"""
-*********************
-**Module**: models.hwm
-*********************
+"""hwm module
 
-**Modules**:
-    * :mod:`hwm14`: fortran subroutines
+Modules
+---------------------------
+hwm14   fortran subroutines
+---------------------------
   
-*********************
 """
 import logging
 
 def hwm14(iyd,sec,alt,glat,glon,stl,f107a,f107,ap,path=None):
+    """
 
+    Parameters
+    ----------
+    iyd :
+
+    sec :
+
+    alt :
+
+    glat :
+
+    glon :
+
+    stl :
+
+    f107a :
+
+    f107 :
+
+    ap :
+
+    path : Optional[str]
+        location for davitpy installation.  Default is rcParams' 'DAVITPY_PATH'
+        value.
+
+    """
     try:
         from hwm14 import hwm14
     except Exception as e:
