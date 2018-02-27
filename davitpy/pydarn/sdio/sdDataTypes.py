4eeda17d4e8de80ac0f389ba19c6897859d34282
ksterne <kevintyler@vt.edu>
2018-02-27 10:40:08 -0500

    Removal of copy over from radDataTypes, or not used here

--- davitpy/pydarn/sdio/sdDataTypes.py
+++ davitpy/pydarn/sdio/sdDataTypes.py
@@ -34,8 +34,6 @@ mapData
 import logging
 from davitpy.utils import twoWayDict
 
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
-         'r','s','t','u','v','w','x','y','z']
 
 class sdDataPtr():
     """A class which contains a pipeline to a data source
