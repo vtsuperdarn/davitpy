188a569ec5ff4d009f46cc5070cefda2d884e4ad
ksterne <kevintyler@vt.edu>
2018-02-27 09:32:43 -0500

    Removing trailing spaces

--- davitpy/pydarn/sdio/sdDataRead.py
+++ davitpy/pydarn/sdio/sdDataRead.py
@@ -1,16 +1,16 @@
 # Copyright (C) 2012  VT SuperDARN Lab
 # Full license can be found in LICENSE.txt
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -120,13 +120,13 @@ def sdDataOpen(stime, hemi='north', eTime=None, src=None, fileName=None,
     -------
     The evironment variables are python dictionary capable formatted strings
     appended encode radar name, channel, and/or date information. Currently
-    supported dictionary keys which can be used are: 
+    supported dictionary keys which can be used are:
 
     'date'    : datetime.datetime.strftime("%Y%m%d")
-    'year'    : 0 padded 4 digit year 
-    'month'   : 0 padded 2 digit month 
-    'day'     : 0 padded 2 digit day 
-    'hour'    : 0 padded 2 digit day 
+    'year'    : 0 padded 4 digit year
+    'month'   : 0 padded 2 digit month
+    'day'     : 0 padded 2 digit day
+    'hour'    : 0 padded 2 digit day
     'ftype'   : filetype string
     'hemi'    : hemisphere
 
@@ -162,7 +162,7 @@ def sdDataReadRec(my_ptr):
     Example
     --------
     ::
-    
+
     import datetime as dt
     my_ptr = sdDataOpen(dt.datetime(2011,1,1), 'south')
     my_data = sdDataReadRec(my_ptr)
@@ -178,7 +178,7 @@ def sdDataReadRec(my_ptr):
     assert isinstance(my_ptr, sdDataPtr), \
         logging.error('input must be of type sdDataPtr')
 
-    return my_ptr.readRec() 
+    return my_ptr.readRec()
 
 def sdDataCreateIndex(my_ptr):
     """A function to index radar data into dict from a sdDataPtr object
@@ -192,7 +192,7 @@ def sdDataCreateIndex(my_ptr):
     ----------
     my_index : (dict)
         Keys are record timedate objects and values are byte offsets into the
-        file. 
+        file.
 
     Example
     --------
@@ -231,7 +231,7 @@ def sdDataReadAll(my_ptr):
     my_list : (list or NoneType)
         A list filled with gridData or mapData objects holding the data we are
         after.  Vill return None if nothing is found.
- 
+
     Examples
     ---------
     ::
